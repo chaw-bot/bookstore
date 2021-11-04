16a599b871243e6c97381dbc072edec2b4b1d0ae
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-04 19:21:15 +0200

    Storage

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import pullBooks from './BookSlice';
 
 const BookList = () => {
-  const bookStore = useSelector((store) => store.booksReducer);
-  const oneBook = bookStore.map((book) => (
-    <Book
-      className="Book"
-      key={book.id}
-      id={book.id}
-      author={book.author}
-      title={book.title}
-      category={book.category}
-    />
-  ));
+  const bStore = [];
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(pullBooks());
+  }, []);
+
+  let bookStore = useSelector((store) => store.booksReducer.books);
+  if (bookStore.error) {
+    bookStore = [];
+  } else {
+    const bookIDs = Object.keys(bookStore);
+    bookIDs.forEach((id) => {
+      bookStore[id].map((book) => (
+        bStore.push(
+          <Book
+            className="Book"
+            key={id}
+            id={id}
+            author={book.author}
+            title={book.title}
+            category={book.category}
+          />,
+        )
+      ));
+    });
+  }
 
   return (
     <div className="bookList">
-      {oneBook}
+      {bStore}
     </div>
   );
 };
