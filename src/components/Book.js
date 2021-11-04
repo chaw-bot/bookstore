9e8ea9d4e3cf61c455cad444dce15ee81b93e16d
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-04 10:52:55 +0200

    Add file extensions

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import BookData from './BookData';
-import Options from './Options';
+import BookData from './BookData.js';
+import Options from './Options.js';
 import Percentage from './Percentage';
 import Progress from './Progress';
 
