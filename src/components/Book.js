194c9ac7e145cb7b54fe93a8bf952cd1b5b0866f
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-04 11:15:43 +0200

    Fix linters

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,3 @@
-// eslint-disable import/extensions
 import React from 'react';
 import PropTypes from 'prop-types';
 import BookData from './BookData';
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './components/bookstore.css';
-// eslint-disable-next-line import/extensions
 import Bookstore from './components/Bookstore';
 
 ReactDOM.render(
