8346faaf6f87f9428aca30e52d088aca5b0fd83b
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-11-04 10:36:12 +0200

    Making storage accessable

--- src/components/bookstore.js
+++ src/components/bookstore.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './navbar';
-import AddBook from './addBook';
-import Categories from './categories';
+import { Provider } from 'react-redux';
+import Navbar from './Navbar';
+import AddBook from './AddBook';
+import Categories from './Categories';
 import BookList from './BookList';
+import store from '../redux/configureStore';
 
 const Bookstore = () => (
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/">
-        <BookList />
-        <AddBook />
-      </Route>
-      <Route path="/categories">
-        <Categories />
-      </Route>
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <BookList />
+          <AddBook />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default Bookstore;
